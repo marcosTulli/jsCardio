f26b56d71f8c8019c6374e3c1ab08941bd75e277
marcosTulli <marcostulli@hotmail.com>
2022-03-21 20:29:32 -0300

    seek and destroy

--- jscardio/14_seek_and_destroy.js
+++ jscardio/14_seek_and_destroy.js
@@ -2,9 +2,19 @@
 // Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
-function seekAndDestroy() {}
+function seekAndDestroy(arr, ...rest) {
+  //   SOLUTION 1
+  //   const args = Array.from(arguments);
+  //   const filterArr = (arr) => {
+  //     return args.indexOf(arr) === -1;
+  //   };
+  //   return arr.filter(filterArr);
+
+  //  SOLUTION 2
+  return arr.filter((val) => !rest.includes(val));
+}
 
 // Call Function
-const output = seekAndDestroy("hello");
+const output = seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6, "hello");
 
 console.log(output);

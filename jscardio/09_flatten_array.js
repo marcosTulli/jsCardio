a000c1060cf2a0e14d739de792579c3742032f9f
marcosTulli <marcostulli@hotmail.com>
2022-03-21 11:14:59 -0300

    flatten array

--- jscardio/09_flatten_array.js
+++ jscardio/09_flatten_array.js
@@ -2,8 +2,16 @@
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
-function flattenArray(arrays) {}
-// Call Function
-const output = flattenArray("hello");
+function flattenArray(arrays) {
+  // SOLUTION 1
+  //   return arrays.reduce((a, b) => a.concat(b));
+  //
+  // SOLUTION 2
+  //
+  //
+  return [].concat.apply([], arrays);
+}
+
+const output = flattenArray([[1, 2], [3, 4], [5, 6], [7]]);
 
 console.log(output);

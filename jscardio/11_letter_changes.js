4e3cea47ada60ca999587050fcc38d05c1db1353
marcosTulli <marcostulli@hotmail.com>
2022-03-21 12:37:50 -0300

    letter Changes

--- jscardio/11_letter_changes.js
+++ jscardio/11_letter_changes.js
@@ -3,9 +3,22 @@
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-function letterChanges(str) {}
+function letterChanges(str) {
+  let newStr = str.toLowerCase().replace(/[a-z]/gi, (char) => {
+    if (char === "z" || char === "Z") {
+      return "a";
+    } else {
+      return String.fromCharCode(char.charCodeAt() + 1);
+    }
+  });
+
+  newStr = newStr.replace(/a|e|i|o|u/gi, (vowel) => {
+    return vowel.toLowerCase();
+  });
+  return newStr;
+}
 
 // Call Function
-const output = letterChanges("hello");
+const output = letterChanges("Selena gOMez");
 
 console.log(output);

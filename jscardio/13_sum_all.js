ae3a1e76d66f146659fea177ef89884e0d6636f0
marcosTulli <marcostulli@hotmail.com>
2022-03-21 16:01:59 -0300

    add all

--- jscardio/13_sum_all.js
+++ jscardio/13_sum_all.js
@@ -2,9 +2,32 @@
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
 
-function addAll() {}
+// ES5 SOLUTION
+// function addAll() {
+//
+//
+//   var args = Array.prototype.slice.call(arguments);
+//   var total = 0;
+//   for (i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
+//   return total;
+//
+// }
+//
+//  SOLUTION 2
+//
 
+function addAll(...nums) {
+  //   let total = 0;
+  //   nums.forEach((num) => (total += num));
+  //   return total;
+
+  //  SOLUTION 3
+
+  return nums.reduce((a, b) => a + b);
+}
 // Call Function
-const output = addAll("hello");
+const output = addAll(2, 5, 6, 7, 12);
 
 console.log(output);

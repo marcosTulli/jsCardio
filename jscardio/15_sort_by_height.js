2613dc88001d18c1b36077e38aaa281702780a6f
marcosTulli <marcostulli@hotmail.com>
2022-03-21 20:42:30 -0300

    sort by height

--- jscardio/15_sort_by_height.js
+++ jscardio/15_sort_by_height.js
@@ -4,9 +4,20 @@
 // a = [-1, 150, 190, 170, -1, -1, 160, 180]
 // sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
 
-function sortByHeight() {}
+function sortByHeight(a) {
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => {
+    val === -1 ? arr1.push(i) : arr2.push(val);
+  });
+  const sortArr = arr2.sort((a, b) => a - b);
+  arr1.forEach((val, i) => sortArr.splice(arr1[i], 0, -1));
+  return sortArr;
+}
 
 // Call Function
-const output = sortByHeight("hello");
+
+const output = sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]);
 
 console.log(output);

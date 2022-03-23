7cbcf46553dedf8d4ff885cb7cdc4cac2e02f672
marcosTulli <marcostulli@hotmail.com>
2022-03-22 23:51:18 -0300

    compare triplets

---
+++ hackerank/02_array_sum.js
@@ -0,0 +1,7 @@
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array  ar = [1,2,3]  1 + 2 + 3 = 6 so return 6 .
+
+function simpleArraySum(ar) {
+  return ar.reduce((a, b) => a + b);
+}
---
+++ hackerank/03_compare_triplets.js
@@ -0,0 +1,33 @@
+// Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+
+// The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+
+// The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+
+// If a[i] > b[i], then Alice is awarded 1 point.
+// If a[i] < b[i], then Bob is awarded 1 point.
+// If a[i] = b[i], then neither person receives a point.
+// Comparison points is the total points a person earned.
+
+// Given a and b, determine their respective comparison points.
+
+function compareTriplets(a, b) {
+  let aPts = 0;
+  let bPts = 0;
+
+  for (let i in a) {
+    if (a[i] > b[i]) {
+      aPts += 1;
+    } else if (b[i] > a[i]) {
+      bPts += 1;
+    }
+  }
+
+  let results = [aPts, bPts];
+  return results;
+}
+
+const alicePoints = [4, 2, 5];
+const bobPoints = [1, 1, 5];
+
+console.log(compareTriplets(alicePoints, bobPoints));

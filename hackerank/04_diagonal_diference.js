94ca5d519caae45c0ac3d58e8af422a3a97b071c
marcosTulli <marcostulli@hotmail.com>
2022-03-24 16:17:11 -0300

    fiexed passed all tests

--- hackerank/04_diagonal_diference.js
+++ hackerank/04_diagonal_diference.js
@@ -10,33 +10,36 @@
 // ABS is 2
 
 function diagonal(arr) {
-  const left = [arr[0][0], arr[1][1], arr[2][2]];
-  const right = [arr[2][0], arr[1][1], arr[0][2]];
-  const sum = (newArr) => {
-    return newArr.reduce((a, b) => a + b);
-  };
-  const absolute = Math.abs(sum(left) - sum(right));
-
-  return absolute;
+  let left = 0;
+  let right = 0;
+  for (let i in arr) {
+    left += arr[i][i];
+    right += arr[i][arr.length - 1 - i];
+  }
+  const value = Math.abs(left - right);
+  return value;
 }
-
 let matrix = [
-  [11, 2, 4],
+  [1, 2, 3],
   [4, 5, 6],
-  [10, 9, -12],
+  [9, 8, 9],
 ];
 console.log(diagonal(matrix));
 
-// const sum = (newArr) => {
-//   newArr.reduce((a, b) => a + b);
-// };
+// function diagonal(arr) {
+//   const left = [arr[0][0], arr[1][1], arr[2][2]];
+//   const right = [arr[2][0], arr[1][1], arr[0][2]];
+//   const sum = (newArr) => {
+//     return newArr.reduce((a, b) => a + b);
+//   };
+//   const absolute = Math.abs(sum(left) - sum(right));
 
-// const x = sum(diagonal(matrix));
-
-// console.log(x);
-
-// function simpleArraySum(ar) {
-//   return ar.reduce((a, b) => a + b);
+//   return absolute;
 // }
 
-// console.log(simpleArraySum([1, 2, 3]));
+// let matrix = [
+//   [11, 2, 4],
+//   [4, 5, 6],
+//   [10, 9, -12],
+// ];
+// console.log(diagonal(matrix));
